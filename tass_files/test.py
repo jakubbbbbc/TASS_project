cfd252761d08a71b5d1e38242c07fd858e61637c
jakubbbbbc <jakub.ciemiega@gmail.com>
2023-01-01 17:40:53 +0100

    saving citation count for each channel source

--- tass_files/test.py
+++ tass_files/test.py
@@ -7,7 +7,10 @@ fname_output = './channels.json'
 with open(fname_output, 'r', encoding='utf-8') as f:
         data = json.load(f)
         print('\n')
+        cite_count = 0
         for key, value in data.items():
+            cite_count += len(value['cited_sources'])
             print(f'{key}:')
             for key2, value2 in value.items():
                 print(f'\t{key2}: {value2}')
+print(cite_count/106)

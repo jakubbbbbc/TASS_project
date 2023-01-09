c78b25a2e829239a168344cd78a8c46faa9a1f88
mjablon1 <01123915@pw.edu.pl>
2023-01-09 23:26:28 +0100

    mode2 updated, mode4 added

--- grouping.py
+++ grouping.py
@@ -1,6 +1,5 @@
 import json
 import numpy as np
-import networkx as nx
 
 
 def json_to_list(path='tass_files/channels.json'):
@@ -25,22 +24,30 @@ def json_to_list(path='tass_files/channels.json'):
         dict['full_name'] = value['channel_name']
         dict['verified'] = value['verified']
         dict['cited_sources'] = value['cited_sources']
+        list_sources = np.asarray(value['cited_sources'])
+        list_sources_count = np.asarray(value['citation_count'])
+        source_count_dict = {}
+        for source, count in zip(list_sources, list_sources_count):
+            source_count_dict[source] = count
+        dict['source_count'] = source_count_dict
+        dict['n_all_citations'] = sum(list_sources_count)
+        dict['n_all_sources'] = len(list_sources)
         list[i] = dict
 
     return list, n_channels
 
 
-def produce_weights_for_network(data, x=1):
+def produce_weights_for_network(data, x=1.):
     """
     2 channels are connected when:
     mode1:
         they cited at least x common sources, weight = 1
     mode2:
-        they both cited more than y percentage of total number of cited sources of the other channel, weight = 1
+        weight > x, weight = number_of_citations_from_common_sources/number_of_all_citations, weight = min(weight1, weight2) for 1., 2. channel accordingly
     mode3:
         they cited at least 1 common source, weight = n_of_common_sources_cited
-    
-    Returns ...
+    mode4:
+        weight > x, weight = number_of_common_sources/number_of_all_sources, weight = min(weight1, weight2) for 1., 2. channel accordingly
     """
     n_channels = len(data)
     for i, record1 in enumerate(data):
@@ -60,20 +67,49 @@ def produce_weights_for_network(data, x=1):
     
     for i, record1 in enumerate(data):
         mode2 = [None] * n_channels
+        mode4 = [None] * n_channels
         for j, record2 in enumerate(data):
-            mode3 = record1['mode3']
-            if mode3[i] == 0:
-                mode2[j] = 0
+            intersection = list(set(record2['cited_sources']).intersection(record1['cited_sources']))
+            n_common = len(intersection)
+            sum1 = 0
+            sum2 = 0
+            dict1 = record1['source_count']
+            dict2 = record2['source_count']
+            for element in intersection:
+                sum1 += dict1[element]
+                sum2 += dict2[element]
+            if record1['n_all_citations'] != 0:
+                percentage1_mode2 = sum1/record1['n_all_citations']
             else:
-                mode2[j] = mode3[j]/mode3[i]
+                percentage1_mode2 = 0
+            if record2['n_all_citations'] != 0:
+                percentage2_mode2 = sum2/record2['n_all_citations']
+            else:
+                percentage2_mode2 = 0
+            mode2[j] = min(percentage1_mode2, percentage2_mode2)
+            if mode2[j] <= x:
+                mode2[j] = 0.
+            
+            if record1['n_all_sources'] != 0:
+                percentage1_mode2 = n_common/record1['n_all_sources']
+            else:
+                percentage1_mode2 = 0
+            if record2['n_all_sources'] != 0:
+                percentage2_mode2 = n_common/record2['n_all_sources']
+            else:
+                percentage2_mode2 = 0
+            mode4[j] = min(percentage1_mode2, percentage2_mode2)
+            if mode4[j] <= x:
+                mode4[j] = 0.
 
         record1['mode2'] = mode2
+        record1['mode4'] = mode4
     
     return data
 
 
 def produce_weight_matrix(data, n_channels):
-    list_modes = ['mode1', 'mode2', 'mode3']
+    list_modes = ['mode1', 'mode2', 'mode3', 'mode4']
     n_modes = len(list_modes)
     matrix = np.zeros((n_channels, n_channels, n_modes))
     for m, mode in enumerate(list_modes):
@@ -105,7 +141,7 @@ def produce_edges(matrix, n, mode_id):
     return edge_list
 
 
-def combined(mode, x=0):
+def combined(mode, x=0.):
     channel_data_list, n = json_to_list()
 
     output_data = produce_weights_for_network(channel_data_list, x)
@@ -119,6 +155,8 @@ def combined(mode, x=0):
 
 # combined(0, 30)
 
-# combined(1)
+# combined(1, 0.85)
 
 # combined(2)
+
+# combined(3, 0.4)
